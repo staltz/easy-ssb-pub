07dcaf4ae5a5631ad3567380e7873acd1db5b8db
Andre Staltz <andre@staltz.com>
2017-03-11 16:34:05 +0200

    Refactor src/index.ts a tiny bit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import {createScuttlebot} from './scuttlebot';
 import {createDiscoveryPeer} from './discovery';
 import {createExpressApp} from './http';
 
-const {bot, config} = createScuttlebot();
-createDiscoveryPeer(bot, config);
-createExpressApp(bot);
+const {ssbBot, ssbConf} = createScuttlebot();
+createDiscoveryPeer(ssbBot, ssbConf);
+createExpressApp(ssbBot);
