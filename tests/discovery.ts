7b050c0008a46a4e42a09c5a660694bff2e504f7
Andre Staltz <andre@staltz.com>
2017-03-13 10:24:14 +0200

    Add placeholder tests for discovery

---
+++ tests/discovery.ts
@@ -0,0 +1,15 @@
+import assert = require('assert');
+
+describe('easy-ssb-pub discovery peer', function () {
+  it('should find and exchange invites with another peer', function (done) {
+    assert.fail('', '', 'TODO', '');
+  });
+
+  it('should ignore peers with older versions', function (done) {
+    assert.fail('', '', 'TODO', '');
+  });
+
+  it('should ignore peers with different discovery id prefix', function (done) {
+    assert.fail('', '', 'TODO', '');
+  });
+});
