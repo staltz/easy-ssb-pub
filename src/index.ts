09267b4b1dcd49f5b0aec72d0edc321fc4e07088
Michael Dougherty <maackle.d@gmail.com>
2018-01-05 23:15:53 -0800

    Add script to try setting profile name on start

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import {setupScuttlebot} from './scuttlebot';
 import {setupDiscoveryPeer} from './discovery';
 import {setupExpressApp} from './http';
+import {setName} from './set-name';
 
 const {ssbBot, ssbConf} = setupScuttlebot();
 setupDiscoveryPeer({bot: ssbBot, config: ssbConf});
 setupExpressApp({bot: ssbBot});
+setName({bot: ssbBot});
